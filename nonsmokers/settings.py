0fd8d6b6ff9aedf7479d997a8449b05f574ec30d
Bahattin Cinic <bahattin@adphorus.com>
2017-10-16 14:50:11 +0300

    added static_root

--- nonsmokers/settings.py
+++ nonsmokers/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
+
+STATICFILES_DIRS = (
+    os.path.normpath(os.path.join(BASE_DIR, 'assets')),
+)
