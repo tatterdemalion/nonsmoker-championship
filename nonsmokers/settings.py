7f5b3c78a8861f68339fc0436bd0edded05fa913
Bahattin Cinic <bahattin@adphorus.com>
2017-10-16 14:51:10 +0300

    removed STATICFILES_DIRS

--- nonsmokers/settings.py
+++ nonsmokers/settings.py
@@ -125,7 +125,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
-
-STATICFILES_DIRS = (
-    os.path.normpath(os.path.join(BASE_DIR, 'assets')),
-)
